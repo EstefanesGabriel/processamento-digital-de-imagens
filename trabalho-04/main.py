8510d569bc02a37e019c74e06ee68dcd4b6b5663
Alexandre Alberto Menon <alexandrealberto.menon@gmail.com>
2025-10-10 20:08:23 -0300

    Trabalho 04 finalizado

--- trabalho-04/main.py
+++ trabalho-04/main.py
@@ -48,12 +48,15 @@ def rices_contour(treated_img):
 
     avg_rice_area = np.median(median_areas)
 
+    desvio = np.std(areas)
+    media = np.mean(areas)
+
+    if desvio / media < 0.25:  # tolerância ajustável
+        return len(contours)
+
     total_rice_count = 0
     for contour in contours:
         area = cv2.contourArea(contour)
-        if area < avg_rice_area * 0.5:
-            continue
-        
         count_in_contour = round(area / (avg_rice_area * FATOR_DE_CALIBRAGEM))
         
         if count_in_contour == 0 and area > avg_rice_area * 0.5:
@@ -80,35 +83,7 @@ def main():
 
 #-------------------------------------------------------------------------------
 
-def find_best_calibre():
-    totais = [60, 82, 114, 150, 205]
-    variacao = []
-    menor_desvio = float('inf')
-    melhor_calibragem = 0
-    print (menor_desvio)
-    for c in range(1, 2000):
-        i = 0
-        print(c)
-        desvio = 0
-        calibragem = round(c/1000, 3)
-        for img in IMAGES:
-            treated_img = image_treatment(img)
-            total_rices = rices_contour(treated_img)
-            print_total_rices(total_rices)
-            desvio += abs(totais[i] - total_rices)
-            i += 1
-        variacao.append((desvio, calibragem))
-        if desvio < menor_desvio:
-            menor_desvio = desvio
-            melhor_calibragem = calibragem
-
-    print("----------------------------------------------")
-    print(len(variacao))
-    print(menor_desvio)
-    print(melhor_calibragem)
-
 if __name__ == "__main__":
-    # main()
     for img in IMAGES:
         treated_img = image_treatment(img)
         total_rices = rices_contour(treated_img)
