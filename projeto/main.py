dae8829d9bc6ab8bfbd514c4bbbdfbd2f2cdb6ca
Alexandre Alberto Menon <alexandrealberto.menon@gmail.com>
2025-12-02 17:16:55 -0300

    Adicionado contagem de plaquetas e células brancas

--- projeto/main.py
+++ projeto/main.py
@@ -23,7 +23,6 @@ IMAGES = [
     'dataset/6.png',
     'dataset/7.png',
     'dataset/8.png',
-    'dataset/9.png',
 ]
 
 #===============================================================================
@@ -62,34 +61,21 @@ def filter_white_cell(img):
 #
 #     return img_dilated
 
-# def platelet_count(treated_img):
-#     contours, _ = cv2.findContours(treated_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#
-#     areas = [cv2.contourArea(c) for c in contours]
-#     areas.sort()
-#
-#     # limite_superior = max(limite_inferior + 1, int(len(areas) * 0.4))
-#     # median_areas = areas[limite_inferior:limite_superior]
-#
-#     avg_rice_area = np.median(median_areas)
-#
-#     desvio = np.std(areas)
-#     media = np.mean(areas)
-#
-#     if desvio / media < 0.25:  # tolerância ajustável
-#         return len(contours)
-#
-#     total_rice_count = 0
-#     for contour in contours:
-#         area = cv2.contourArea(contour)
-#         count_in_contour = round(area / (avg_rice_area * FATOR_DE_CALIBRAGEM))
-#
-#         if count_in_contour == 0 and area > avg_rice_area * 0.5:
-#             count_in_contour = 1
-#
-#         total_rice_count += count_in_contour
-#
-#     return total_rice_count
+def blob_count(treated_img):
+    contours, _ = cv2.findContours(treated_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    areas = [cv2.contourArea(c) for c in contours]
+    areas.sort(reverse=True)
+
+    white = -1
+    
+    for i in range(len(areas) - 1):
+        if areas[i] > areas[i+1] * 4:
+            white = i + 1
+            break
+
+    platelet = len(areas) - white
+    return white, platelet
 
 if __name__ == '__main__':
     if not os.path.exists("resultados"):
@@ -100,7 +86,13 @@ if __name__ == '__main__':
             print(f"Erro abrindo a imagem: {IMAGES[i]}\n")
             continue
 
-        result = filter_white_cell(img.copy())
+        binary = filter_white_cell(img.copy())
+        white, platelet = blob_count(binary.copy())
 
+        print("=============")
+        print(f"Imagem {i}")
+        print("=============")
+        print(f"Células brancas: {white}")
+        print(f"Células platelet: {platelet}")
         out_path = os.path.join("resultados", str(i) + ".png")
-        cv2.imwrite(out_path, result)
+        cv2.imwrite(out_path, binary)
